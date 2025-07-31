308f460c7654ea69c28202964143b8934bbcf3c6
CaptainZiboo <96137708+CaptainZiboo@users.noreply.github.com>
2025-07-31 22:06:58 +0200

    teeeeeeeeeest

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "lambda_function_over_https" {
   function_name    = "TodosLambdaFunction"
   role             = aws_iam_role.lambda_apigateway_role.arn
   handler          = "index.handler"
-  source_code_hash = data.archive_file.lambda_archive.output_base64sha256
+  source_code_hash = filebase64sha256("${path.module}/lambda/lambda_function_payload.zip")
   runtime          = "nodejs20.x"
   
   environment {
